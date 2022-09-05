fb9fbd02919debcbbc47428b5da25e2e46bc18e6
Thayara Siqueira <thay.siqu@gmail.com>
2022-09-05 12:05:40 -0300

    feat: creates model for users

---
+++ src/database/models/user.js
@@ -0,0 +1,21 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'blogposts' });
+  }
+
+  return User;
+};
+
+module.exports = User;
--- src/database/models/users.js
+++
@@ -1,9 +0,0 @@
-const User = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    displayName: DataTypes.STRING,
-  });
-
-  return User;
-};
-
-module.exports = User;
